bb64b63dd29b4820b985edd4eece79d2b65c63e4
nicogite <ncls.legendre@mail.com>
2026-06-12 11:50:24 +0200

    Update icons. Fix manifest.xml for notifs and orientation

--- ios/App/App/public/assets/index-DJAOM1bz.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B3t9CEmd.js","assets/index-ChejtLU2.js"])))=>i.map(i=>d[i]);
-import{a3 as r,a4 as a}from"./index-ChejtLU2.js";const o=r("Share",{web:()=>a(()=>import("./web-B3t9CEmd.js"),__vite__mapDeps([0,1])).then(e=>new e.ShareWeb)});export{o as S};
